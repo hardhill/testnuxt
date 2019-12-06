9e264a83db8d7a06c15d4b19c2d05d560ef55a3b
hardhill <hardhill@mail.ru>
2019-12-06 14:54:38 +0800

    Update admin.vue

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,10 +1,27 @@
 <template>
-    <v-container>
-        <aside width="200px">
-
+    <v-content class="amain-layout-wrap">
+    <div class="container">
+        <aside class="sidebar">
+            <div>Aside</div>
         </aside>
         <main>
             <nuxt/>
         </main>
-    </v-container>
+    </div>
+    </v-content>
 </template>
+<style scoped>
+.sidebar{
+    width: 200px;
+}
+.amain-layout-wrap{
+    height: 100vh;
+    width: 100%;
+}
+.container{
+    display: flex;
+    padding: 0px;
+    margin: 0px;
+    height: 100vh;
+}
+</style>
